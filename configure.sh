aa98c38b51d6e302664c25723a4fea2a34b1f5cb
Your Name <you@example.com>
2023-02-10 03:47:32 +0200

    modified pushd to cd

--- configure.sh
+++ configure.sh
@@ -5,10 +5,10 @@ psw=bindecy
 # install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh ./get-docker.sh
-pushd docker
+cd docker
 docker build -t bindecy:1.0 .
 docker run -d --name jenkins-bindecy -p 8080:8080 jenkins-bindecy:1.0
-popd
+cd ..
 
 read -p "Waiting a bit for docker instance to startup.." -t 7
 
@@ -17,7 +17,7 @@ crumb=$(curl -v -X GET http://localhost:8080/crumbIssuer/api/json --user $user:$
 -H "Cookie: JSESSIONID.56e4b3b7=node0xsbnd4uu2dt41j69vxgq0zfml1.node0; JSESSIONID.fd4e3cc2=node0sv2b0t44x0wq10eyfiohqnomq22.node0" | jq -r '.crumb')
 
 #configure secrets
-pushd jenkins/creds
+cd jenkins/creds
 for file in *
 do
 data=$(cat $file)
@@ -27,9 +27,9 @@ curl -X POST http://localhost:8080/manage/credentials/store/system/domain/_/crea
     -H "Jenkins-Crumb: $crumb" \
     --data-raw "$data"
 done
-popd 
+cd ../.. 
 
-pushd jenkins/pipelines
+cd jenkins/pipelines
 for file in *
 do
 data=$(cat $file)
@@ -39,5 +39,5 @@ curl -X POST http://localhost:8080/createItem?name=${file%.*} --user $user:$psw 
     -H "Jenkins-Crumb: $crumb" \
     --data "$data"
 done
-popd
+cd ../..
 echo $crumb
