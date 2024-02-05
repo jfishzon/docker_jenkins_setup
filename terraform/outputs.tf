89997058312f659b7e71537c8747d15930e4e94b
jfishzon <64330065+jfishzon@users.noreply.github.com>
2024-02-05 11:53:53 +0200

    modified outputs to correct instance name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
 output "instance_id" {
     description = "instance id of created server"
-    value       = aws_instance.bindecy
+    value       = aws_instance.ssh_host
 }
 output "instance_public_ip" {
     description = "public ip of instance"
-    value = aws_instance.bindecy.public_ip
+    value = aws_instance.ssh_host.public_ip
 }
